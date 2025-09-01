296e4636a461efa7e54c02157f7b32c3a3184e00
zizo0022sasa <zizo00220033@gmail.com>
2025-09-01 11:00:40 +0300

    Delete start_simple_bot.sh

--- start_simple_bot.sh
+++
@@ -1,12 +0,0 @@
-#!/bin/bash
-echo "🔥 تشغيل البوت البسيط..."
-echo "========================"
-
-# تثبيت المتطلبات
-pip3 install -r requirements_simple.txt -q
-
-# إنشاء الملفات المطلوبة
-touch accounts.json stats.json bot.log
-
-# تشغيل البوت
-python3 simple_telegram_bot.py
